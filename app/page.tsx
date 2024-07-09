8df36b98ddf54e2575dc46ec87521913a3464b59
xiangmian <yqj741452@outlook.com>
2024-07-09 15:27:41 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-50">
+      <h2 className="text-3xl font-medium">Hello world</h2>
+    </div>
+  )
+}
